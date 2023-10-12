fc439343ccf6bbfd6e0ff3e6841755847f1462e9
Nagaxd1 <osasenaga2019@gmail.com>
2023-10-13 00:28:08 +0200

    Added aws credentials again

--- inc/dbcon.php
+++ inc/dbcon.php
@@ -1,10 +1,11 @@
 <?php
 $host = "wash.cznay1hciiaq.eu-north-1.rds.amazonaws.com";
+$port = 3306;
 $username = "root";
 $dbname = "wash";
-$password = "qwerty";
+$password = "qwerty1234";
 
-$conn = mysqli_connect($host,$username, $password, $dbname);
+$conn = mysqli_connect($host,$username, $password, $dbname, $port);
 
 if(!$conn) {
 
