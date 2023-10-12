0230355357fb3cdfffa773a8c059c5bc15e5eecd
Nagaxd1 <osasenaga2019@gmail.com>
2023-10-12 12:27:52 +0200

    Added a test html page

---
+++ devices/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>API Test</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    
+    <button><a href="read.php">READ></a></button>
+
+    <button><a href="readALL.php">READALL></a></button>
+
+    <button><a href="create.php">CREATE></a></button>
+
+    <button><a href="update.php">UPDATE></a></button>
+
+    <button><a href="delete.php">DELETE></a></button>
+</body>
+</html>
